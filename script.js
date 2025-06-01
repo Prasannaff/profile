e619389e073b210cd1166f3ccd495f45e8d4ef0c
Prasanna Pattar <prasannapattar07@gmail.com>
2025-06-01 19:36:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+// particlesJS.load('particles-js', 'particles.json', function() {
+//   console.log('particles.js loaded');
+// });
+var typed = new Typed("#typing", {
+  strings: ["Web Developer", "Java Enthusiast", "Spring Boot Developer"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true
+});
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+document.getElementById("darkModeToggle").addEventListener("change", function () {
+  document.body.classList.toggle("dark-mode");
+});
